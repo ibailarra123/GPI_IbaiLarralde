cae0da3d9e4b2c18b31de0ddf0df87cbdde20960
Ibai Larralde <125909936+ibailarra123@users.noreply.github.com>
2024-05-23 16:38:50 +0200

    Update ejecutarTodo.sh

--- ejecutarTodo.sh
+++ ejecutarTodo.sh
@@ -4,3 +4,11 @@ mvn clean verify
 # Ejecutar pruebas
 MAVEN_OPTS="--illegal-access=warn" mvn test 2>&1 | tee test.log
 
+# Clonar el repositorio de easybuggy
+git clone https://github.com/k-tamura/easybuggy.git
+
+# Entrar en el directorio de easybuggy
+cd easybuggy
+
+# Ejecutar el script de compilación (si lo proporciona el proyecto easybuggy)
+mvn clean install
